bbc59d897717d902d15ca4d16af8e6d1e5d55d95
HRahman1777 <hasiburrahman1313@gmail.com>
2021-09-30 19:36:04 +0600

    Create InheritanceTest.cpp

---
+++ Algorithm_DataStructure/OOP/InheritanceTest.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Vehicle
+{
+public:
+    string brandname = "Ford";
+public :
+    void honk()
+    {
+        cout<<"tuut tuuut!"<<endl;
+    }
+};
+
+class Car: public Vehicle
+{
+public:
+    string model = "Mustang";
+};
+
+int main()
+{
+    Car car1;
+    car1.honk();
+    cout<<car1.brandname + " - " + car1.model<<endl;
+
+    return 0;
+}
