09e47850d8d8ba7f7e37215459f6efbc04321235
HRahman1777 <hasiburrahman1313@gmail.com>
2021-09-30 19:35:55 +0600

    Update BubbleSort.cpp

--- Algorithm_DataStructure/BubbleSort.cpp
+++ Algorithm_DataStructure/BubbleSort.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 void bubble_sort(int a[], int l)
 {
-    int temp, x;
-    for(int i=0; i<l; i++)
+    int temp;
+    int i, j;
+    for (i=0; i<l-1; i++)
     {
-        for(int ii=i; ii>0; ii--)
+        for (j=0; j<l-i-1; j++)
         {
-           if(a[ii]<a[ii-1])
-           {
-               temp = a[ii];
-               a[ii] = a[ii-1];
-               a[ii-1] = temp;
-           }
+            if(a[j] > a[j+1])
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
         }
     }
 }
