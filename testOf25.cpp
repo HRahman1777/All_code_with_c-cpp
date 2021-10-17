705b4edb4b84e3fb1f6498290911e6abe1f1308e
HRahman1777 <hasibur.cse7@gmail.com>
2021-10-17 11:45:10 +0600

    test

---
+++ testOf25.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, cnt;
+    for(int i=1; i<300; i++)
+        cout<<i*25<<endl;
+
+
+    return 0;
+}
+
---
+++ test_adsString.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string ok[] = {"00", "25", "50", "75"};
+    string s = {"abcde"};
+
+    cout<<ok[0][0]<<endl;
+    cout<<ok[0][1]<<endl;
+
+    cout<<ok[1][0]<<endl;
+    cout<<ok[1][1]<<endl;
+
+
+
+    cout<<s[1]<<endl;
+
+
+    return 0;
+}
+
