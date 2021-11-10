5c6fdad4370b2b17f8e372a8cf07f491916ee33e
HRahman1777 <hasibur.cse7@gmail.com>
2021-11-10 21:56:36 +0600

    New Year Chaos - Day4.

---
+++ HackerRank/interview pre/hr-NewYearChaos_d4.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void minimumBribes(vector<int> q, int n)
+{
+    int flag = 0, res = 0;
+    int i, j;
+    for (i = n - 1; i >= 0; i--)
+    {
+        if (q[i] - (i + 1) > 2)
+        {
+            cout << "Too chaotic\n";
+            return;
+        }
+        int j = i;
+        while (j < n - 1 && q[j] > q[j + 1])
+        {
+            int temp = q[j];
+            q[j] = q[j + 1];
+            q[j + 1] = temp;
+            j++;
+            res++;
+        }
+    }
+    cout << res << "\n";
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vector<int> q(n);
+        for (int i = 0; i < n; i++)
+        {
+            int q_item;
+            cin >> q_item;
+            q[i] = q_item;
+        }
+
+        minimumBribes(q, n);
+    }
+
+    return 0;
+}
