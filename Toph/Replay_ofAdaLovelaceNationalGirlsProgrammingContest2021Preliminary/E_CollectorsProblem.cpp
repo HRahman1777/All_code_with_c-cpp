4ac75879ca887e41d9d4791d2de9df1f0b9a5d7f
HRahman1777 <hasibur.cse7@gmail.com>
2021-12-02 23:37:00 +0600

    E Collectors Problem - wrong answer. need to recheck

--- Toph/Replay_ofAdaLovelaceNationalGirlsProgrammingContest2021Preliminary/E_CollectorsProblem.cpp
+++ Toph/Replay_ofAdaLovelaceNationalGirlsProgrammingContest2021Preliminary/E_CollectorsProblem.cpp
@@ -3,62 +3,55 @@ using namespace std;
 typedef long long ll;
 int main()
 {
-    vector<int> v(5);
-    for (int i = 0; i < 5; i++)
+
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> people(n), pack(m), pay(m);
+    for (ll i = 0; i < m; i++)
     {
-        cin >> v[i];
+        cin >> pack[i];
+    }
+    for (ll i = 0; i < m; i++)
+    {
+        cin >> pay[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> people[i];
     }
 
-    cout << *max_element(v.begin(), v.end());
+    vector<ll> sum(n);
 
-    // ll n, m;
-    // cin >> n >> m;
-    // vector<ll> people(n), pack(m), pay(m);
-    // for (ll i = 0; i < m; i++)
-    // {
-    //     cin >> pack[i];
-    // }
-    // for (ll i = 0; i < m; i++)
-    // {
-    //     cin >> pay[i];
-    // }
-    // for (ll i = 0; i < n; i++)
-    // {
-    //     cin >> people[i];
-    // }
-
-    // vector<ll> sum(n);
-
-    // for (ll i = 0; i < m; i++)
-    // {
-    //     ll s = 0;
-    //     for (ll j = 0; j < n; j++)
-    //     {
-    //         s += (people[j] - pack[i]);
-    //     }
-    //     sum[i] = s - pay[i];
-    // }
-    // ll max;
-    // if (m == 1)
-    // {
-    //     max = sum[0];
-    // }
-    // else
-    // {
-    //     max = *max_element(sum.begin(), sum.end());
-    // }
-    // if (max > 0)
-    // {
-    //     cout << "Profit " << max << "\n";
-    // }
-    // else if (max < 0)
-    // {
-    //     cout << "Loss " << max * (-1) << "\n";
-    // }
-    // else
-    // {
-    //     cout << "Neutral\n";
-    // }
+    for (ll i = 0; i < m; i++)
+    {
+        ll s = 0;
+        for (ll j = 0; j < n; j++)
+        {
+            s += (people[j] - pack[i]);
+        }
+        sum[i] = s - pay[i];
+    }
+    ll max;
+    if (m == 1)
+    {
+        max = sum[0];
+    }
+    else
+    {
+        max = *max_element(sum.begin(), sum.end());
+    }
+    if (max > 0)
+    {
+        cout << "Profit " << max << "\n";
+    }
+    else if (max < 0)
+    {
+        cout << "Loss " << max * (-1) << "\n";
+    }
+    else
+    {
+        cout << "Neutral\n";
+    }
 
     return 0;
 }
