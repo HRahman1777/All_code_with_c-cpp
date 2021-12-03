9b226adb342ee763ed51484570bb9d87c4cdaf40
HRahman1777 <hasibur.cse7@gmail.com>
2021-12-03 20:21:04 +0600

    E Collector Problem - solve

--- Toph/Replay_ofAdaLovelaceNationalGirlsProgrammingContest2021Preliminary/E_CollectorsProblem.cpp
+++ Toph/Replay_ofAdaLovelaceNationalGirlsProgrammingContest2021Preliminary/E_CollectorsProblem.cpp
@@ -15,43 +15,66 @@ int main()
     {
         cin >> pay[i];
     }
+    ll sum = 0;
     for (ll i = 0; i < n; i++)
     {
         cin >> people[i];
+        sum += people[i];
     }
 
-    vector<ll> sum(n);
+    ll minv = n * pack[0] + pay[0];
+    for (ll i = 1; i < m; i++)
+    {
+        minv = min(minv, (n * pack[i] + pay[i]));
+    }
 
-    for (ll i = 0; i < m; i++)
+    ll res = sum - minv;
+
+    if (res > 0)
     {
-        ll s = 0;
-        for (ll j = 0; j < n; j++)
-        {
-            s += (people[j] - pack[i]);
-        }
-        sum[i] = s - pay[i];
+        cout << "Profit " << res << "\n";
     }
-    ll max;
-    if (m == 1)
+    else if (res < 0)
     {
-        max = sum[0];
-    }
-    else
-    {
-        max = *max_element(sum.begin(), sum.end());
-    }
-    if (max > 0)
-    {
-        cout << "Profit " << max << "\n";
-    }
-    else if (max < 0)
-    {
-        cout << "Loss " << max * (-1) << "\n";
+        cout << "Loss " << res * (-1) << "\n";
     }
     else
     {
         cout << "Neutral\n";
     }
+
+    // vector<ll> sum(n);
+
+    // for (ll i = 0; i < m; i++)
+    // {
+    //     ll s = 0;
+    //     for (ll j = 0; j < n; j++)
+    //     {
+    //         s += (people[j] - pack[i]);
+    //     }
+    //     sum[i] = s - pay[i];
+    // }
+    // ll max;
+    // if (m == 1)
+    // {
+    //     max = sum[0];
+    // }
+    // else
+    // {
+    //     max = *max_element(sum.begin(), sum.end());
+    // }
+    // if (max > 0)
+    // {
+    //     cout << "Profit " << max << "\n";
+    // }
+    // else if (max < 0)
+    // {
+    //     cout << "Loss " << max * (-1) << "\n";
+    // }
+    // else
+    // {
+    //     cout << "Neutral\n";
+    // }
 
     return 0;
 }
