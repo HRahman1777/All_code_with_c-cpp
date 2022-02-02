0e5cf249ae3f0a293728dc6c5ffc616312270eca
HRahman1777 <hasibur.cse7@gmail.com>
2022-02-02 22:28:46 +0600

    Badminton Serves

---
+++ CodeChef/BADMINTON.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int tc;
+	cin>>tc;
+	while(tc--){
+	    long long int pp;
+	    cin>>pp;
+	    cout<<(pp/2)+1<<"\n";
+	}
+	
+	return 0;
+}
